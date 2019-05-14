3ee2b30c43650226b0fe226c550a363627f66d42
Eugene Chung <eugene.chung@mezocliq.com>
2019-05-14 13:56:08 -0400

    Initial checkin

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ScrollViewExample from './scroll_view.js';
+import {Text, Image, View, StyleSheet, ScrollView} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+      <ScrollViewExample/>
+    </View>
+  )
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,6 @@
+To start simulator:
+(make sure to start AVD - click on "play" to start emulator)
+
+react-native run-android
+
+(loads on emulator)
